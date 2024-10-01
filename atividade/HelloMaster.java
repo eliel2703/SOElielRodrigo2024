2da50a45ac01719ba7f35e185a0b2382ac55739c
eliel2703 <elielrodrigocordeiro@gmail.com>
2024-10-01 10:00:41 -0300

    01/10

--- atividade/HelloMaster.java
+++ atividade/HelloMaster.java
@@ -4,10 +4,10 @@ import modelo.HelloFromThread;
 
 public class HelloMaster {
     public void letsGetSomeHello(){
-        Thread t1 = new Thread(new HelloFromThread());
-        Thread t2 = new Thread(new HelloFromThread());
-        Thread t3 = new Thread(new HelloFromThread());
-        Thread t4 = new Thread(new HelloFromThread());
+        Thread t1 = new Thread(new HelloFromThread(1));
+        Thread t2 = new Thread(new HelloFromThread(2));
+        Thread t3 = new Thread(new HelloFromThread(3));
+        Thread t4 = new Thread(new HelloFromThread(4));
         t1.start();
         t2.start();
         t3.start();
--- modelo/HelloFromThread.java
+++ modelo/HelloFromThread.java
@@ -2,9 +2,15 @@ package modelo;
 
 public class HelloFromThread implements Runnable {
 
+    private int tId;
+
+    public HelloFromThread(int tId){
+        this.tId = tId;
+    }
+
     @Override
     public void run() {
-        System.out.println("Hello from Thread");
+        System.out.println("Hello from Thread " + tId + "!");
     }
 
 }
