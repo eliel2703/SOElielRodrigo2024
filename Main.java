8c623239547a884e015b155d0138a8f295443c9f
eliel2703 <elielrodrigocordeiro@gmail.com>
2024-10-01 09:46:31 -0300

    01/10

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+import atividade.HelloMaster;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        HelloMaster hm = new HelloMaster();
+        hm.letsGetSomeHello();
     }
-
-
 }
--- atividade/HelloMaster.java
+++ atividade/HelloMaster.java
@@ -1,7 +1,13 @@
 package atividade;
 
+import modelo.HelloFromThread;
+
 public class HelloMaster {
     public void letsGetSomeHello(){
-
+        Thread t1 = new Thread(
+            new HelloFromThread()
+        );
+        t1.start();
+        
     }
 }
