eb9a29aed5a806d36f6dba50d088598105daa122
eliel2703 <elielrodrigocordeiro@gmail.com>
2024-10-08 09:10:42 -0300

    08/10

--- aula02/atividade/HelloMaster.java
+++ aula02/atividade/HelloMaster.java
@@ -4,10 +4,9 @@ import modelo.HelloFromThread;
 
 public class HelloMaster {
     public void letsGetSomeHello(){
-        for (int i = 1; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             Thread t = new Thread(new HelloFromThread(i));
             t.start();
         }
-
     }
 }
