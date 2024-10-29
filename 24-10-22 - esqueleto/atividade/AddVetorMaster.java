64924312a2a4ad695b1ce080b379898fc52f535b
eliel2703 <159945325+eliel2703@users.noreply.github.com>
2024-10-29 07:09:56 -0300

    Update AddVetorMaster.java

--- 24-10-22 - esqueleto/atividade/AddVetorMaster.java
+++ 24-10-22 - esqueleto/atividade/AddVetorMaster.java
@@ -4,41 +4,59 @@ import modelo.AddVetorThread;
 
 public class AddVetorMaster {
     // definir atributos
+    private double[] a, b, r;
+    private int numThreads;
+    private AddVetorThread[] threads;
 
-    public AddVetorMaster(
-        // inserir parâmetros
-    ) {
-        // alocar parâmetros
+    public AddVetorMaster(double[] a, double[] b, int numThreads) {
+        this.a = a;
+        this.b = b;
+        this.numThreads = numThreads;
+        this.r = new double[a.length];
+        this.threads = new AddVetorThread[numThreads];
+        
+        // Alocar e iniciar threads
+        for (int i = 0; i < numThreads; i++) {
+            int inicio = calcInicio(i);
+            int fim = calcFim(i);
+            threads[i] = new AddVetorThread(a, b, r, inicio, fim);
+        }
     }
 
     public void calcVetores() {
-        // ver slides 14 e 15
+        // Iniciar todas as threads
+        for (AddVetorThread thread : threads) {
+            thread.start();
+        }
+        joinThreads(); // Esperar todas as threads terminarem
     }
 
     public double[] getVetorR() { // este é fácil!!!
         return r;
     }
 
-    private int calcInicio(
-    // inserir parâmetros
-    ) {
-
-        // temporário
-        return -1;
+    private int calcInicio(int i) {
+        // Divide o trabalho entre as threads
+        return i * (a.length / numThreads);
     }
 
-    private int calcFim(
-    // inserir parâmetros
-    ) {
-
-        // temporário
-        return -1;
+    private int calcFim(int i) {
+        // Define o fim da parte do vetor para a thread atual
+        if (i == numThreads - 1) {
+            return a.length; // Última thread vai até o final
+        } else {
+            return (i + 1) * (a.length / numThreads);
+        }
     }
-
-    
 
     private void joinThreads() {
-    // objetivo da aula!
+        // Espera todas as threads terminarem
+        for (AddVetorThread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
